5b8aa1f92d7749d8d2e5e15fe7ff2672dd88c762
Jorge Castillo <jorge.castillo.prz@gmail.com>
2020-07-03 16:50:46 +0200

    Revert "Use Either.catch instead of try catch."
    This reverts commit 27d274388c2ec82d4f254b465240385ae5b6a06d.

--- arrow-ank/src/main/kotlin/arrow/ank/ank.kt
+++ arrow-ank/src/main/kotlin/arrow/ank/ank.kt
@@ -6,8 +6,6 @@ import arrow.core.extensions.list.traverse.sequence
 import arrow.core.extensions.nonemptylist.semigroup.semigroup
 import arrow.core.extensions.validated.applicative.applicative
 import arrow.core.fix
-import arrow.core.nel
-import arrow.core.toT
 import arrow.core.invalidNel
 import arrow.core.toT
 import arrow.core.validNel
